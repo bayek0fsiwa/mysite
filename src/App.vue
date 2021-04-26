813e520e26abb8b75ca2754ab9cff750b6e1f85a
bayek0fsiwa <imvikas@tutanota.com>
2021-04-26 15:13:30 +0530

    transition between routes

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,14 @@
   <v-app dark>
     <Layout />
     <v-content class="mx-4 mb-4">
-       <router-view />
+       <!-- <router-view v-slot="{ Component }">
+         <transition name="route" mode="out-in">
+         <component :is="Component"></component>
+         </transition>
+       </router-view> -->
+       <transition name="route" mode="out-in">
+          <router-view></router-view>
+       </transition>
     </v-content> 
   </v-app>
 </template>
@@ -18,3 +25,20 @@ export default {
   }),
 };
 </script>
+
+<style scoped>
+.route-enter-from {
+  opacity: 0;
+  transform: translateX(100px);
+}
+.route-enter-active {
+  transition: all 0.3s ease-out;
+}
+.route-leave-to {
+  opacity: 0;
+  transform: translateX(-100px);
+}
+.route-leave-active {
+  transition: all 0.3s ease-in;
+}
+</style>
